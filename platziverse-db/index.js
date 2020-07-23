ff264698f826ccbc2a232e38d38fe8d9d32d6a31
emanuelosva <emanuelosva@gmail.com>
2020-07-23 09:27:44 -0500

    Module first integration

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,22 @@
 'use strict';
 
-module.exports = (config) => {
+const setupDatabase = require('./lib/db');
+const setupAgentModel = require('./models/agent');
+const setupMetricModel = require('./models/metric');
+
+module.exports = async (config) => {
+  // Setup for db and Models
+  const sequelize = setupDatabase(config);
+  const AgentModel = setupAgentModel(config);
+  const MetricModel = setupMetricModel(config);
+
+  // Define relations and foregein keys
+  AgentModel.hasMany(MetricModel);
+  MetricModel.belongsTo(AgentModel);
+
+  // Test connection
+  await sequelize.authenticate();
+
   const Agent = {};
   const Metric = {};
 
