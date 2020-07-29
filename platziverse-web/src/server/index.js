2f9f20a67515420bcba60ffb3797b06806dc9d89
emanuelosva <emanuelosva@gmail.com>
2020-07-29 18:07:43 -0500

    Socket io first config

--- platziverse-web/src/server/index.js
+++ platziverse-web/src/server/index.js
@@ -1,19 +1,37 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const chalk = require('chalk');
 const { join } = require('path');
-const { handleError } = require('platziverse-utils');
+const { handleError, createLogger } = require('platziverse-utils');
 
 const port = process.env.PORT || 8080;
+const logger = createLogger('platziverse:web');
 
 // --- Server ---
-const server = express();
-server.use(express.static(join(__dirname, 'public')));
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.disable('x-powered-by');
+
+app.use(express.static(join(__dirname, 'public')));
 // --- Server ---
+
+// Socket.io Websockets ---
+io.on('connect', (socket) => {
+  logger('Socket connected Id:', socket.id);
+
+  socket.on('agent/message', (payload) => {
+    logger('Client message', payload);
+  });
+});
+// Socket.io Websockets ---
 
 // --- Server initialization ---
 process.on('unhandledRejection', handleError.fatal);
 
 server.listen(port, (err) => {
   if (err) return new Error('Server Error');
-  console.log(`${chalk.greenBright('platziverse:web')} listen on port: ${port}`);
+  console.log(`${chalk.blueBright('platziverse:web')} listen on port: ${port}`);
 });
