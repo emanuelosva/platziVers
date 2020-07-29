818d3d73c19533199f46bd14724b851e283bd131
emanuelosva <emanuelosva@gmail.com>
2020-07-29 12:34:21 -0500

    Fix middleware for catch correct status

--- platziverse-api/src/middlewares/errorHandler.js
+++ platziverse-api/src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const response = require('./response');
 
 const errorHandler = (err, req, res, next) => {
-  response.error(req, res, err.statusCode, err);
+  response.error(req, res, err.statusCode || err.status, err);
 };
 
 module.exports = errorHandler;
